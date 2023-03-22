a6e60abbd1b01e06484221c72e776e77b1332144
Ethan Barker <ethanbarker0@gmail.com>
2023-03-22 15:36:39 -0700

    add PDF generator

--- app/src/main/java/com/example/foodie/ui/RecipeDetailActivity.kt
+++ app/src/main/java/com/example/foodie/ui/RecipeDetailActivity.kt
@@ -1,25 +1,29 @@
 package com.example.foodie.ui
 
 import android.content.Intent
+import android.graphics.Paint
+import android.graphics.Typeface
+import android.graphics.pdf.PdfDocument
+import android.graphics.pdf.PdfDocument.PageInfo
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
-import android.view.MenuInflater
 import android.view.MenuItem
-import android.view.View
-import android.widget.TextView
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.navArgs
+import androidx.core.content.FileProvider
+import com.bumptech.glide.Glide
 import com.example.foodie.R
 import com.example.foodie.data.Recipe
-import com.bumptech.glide.Glide
+import java.io.File
+import java.io.FileOutputStream
+
 
 const val EXTRA_FAVORITE_RECIPE = "FAVORITE_RECIPE"
+
 class RecipeDetailActivity : AppCompatActivity() {
     private var isFavorited = false
     private var recipes: Recipe? = null
@@ -46,12 +50,11 @@ class RecipeDetailActivity : AppCompatActivity() {
             findViewById<TextView>(R.id.tv_recipe_time).text = recipes!!.readyInMinutes.toString()
 
 
-
         }
     }
 
 
-    override fun onCreateOptionsMenu(menu: Menu?) : Boolean {
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.activity_favorite_detail, menu)
 
         val favorite = menu?.findItem(R.id.action_favorite)
@@ -79,7 +82,7 @@ class RecipeDetailActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId) {
+        return when (item.itemId) {
             R.id.action_share -> {
                 shareRecipe()
                 true
@@ -95,17 +98,67 @@ class RecipeDetailActivity : AppCompatActivity() {
     private fun toggleFavoriteRecipe() {
         when (isFavorited) {
             false -> viewModel.addFavoritedRecipe(recipes!!)
-            true ->  viewModel.removeFavoritedRecipe(recipes!!)
+            true -> viewModel.removeFavoritedRecipe(recipes!!)
         }
     }
 
     private fun shareRecipe() {
-            val text = getString(R.string.share_text, recipes!!.title)
-            val intent: Intent = Intent().apply {
-                action = Intent.ACTION_SEND
-                putExtra(Intent.EXTRA_TEXT, text)
-                type = "text/plain"
-            }
-            startActivity(Intent.createChooser(intent, null))
+        val uri = generatePDF()
+        this.grantUriPermission("android", uri, Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        val intent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_STREAM, uri)
+            type = "application/pdf"
         }
+        startActivity(Intent.createChooser(intent, null))
+    }
+
+    private fun generatePDF(): Uri {
+        val document = PdfDocument()
+        val pageInfo = PageInfo.Builder(794, 1123, 1).create()
+        val page = document.startPage(pageInfo)
+
+        val textPaint = Paint()
+        textPaint.typeface = Typeface.DEFAULT
+        textPaint.textSize = 34F
+
+        val canvas = page.canvas
+        val title = recipes!!.title
+        var yPos = 100F
+        var xPos = 100F
+
+        canvas.drawText("Foodie Recipe: $title", xPos, yPos, textPaint)
+        yPos += 50
+
+        textPaint.textSize = 20F
+
+        textPaint.typeface = Typeface.DEFAULT_BOLD
+        canvas.drawText("Ingredients:",xPos, yPos, textPaint)
+        textPaint.typeface = Typeface.DEFAULT
+        yPos += 30
+        for (ingredient in recipes!!.ingredients) {
+            canvas.drawText("- ${ingredient.name}", xPos, yPos, textPaint)
+            yPos += 30
+        }
+
+        yPos += 20
+        textPaint.typeface = Typeface.DEFAULT_BOLD
+        canvas.drawText("Steps:", xPos, yPos, textPaint)
+        textPaint.typeface = Typeface.DEFAULT
+        yPos += 30
+        for (step in recipes!!.instructionSets[0].steps) {
+            canvas.drawText("${step.number}. ${step.step}", xPos, yPos, textPaint)
+            yPos += 30
+        }
+        document.finishPage(page)
+        val file = File(applicationContext.filesDir.path, "recipe.pdf")
+        document.writeTo(FileOutputStream(file))
+        val uri = FileProvider.getUriForFile(
+            this,
+            "com.example.foodie.provider",
+            file
+        )
+        document.close();
+        return uri
+    }
 }
